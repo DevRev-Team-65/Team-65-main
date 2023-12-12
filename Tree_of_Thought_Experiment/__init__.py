f51e870e225cef039807e593b22930123c1889eb
swayambhu2104 <122199947+swayambhu2104@users.noreply.github.com>
2023-12-12 16:48:39 +0530

    Update __init__.py

--- Tree_of_Thought_Experiment/__init__.py
+++ Tree_of_Thought_Experiment/__init__.py
@@ -1,6 +1,6 @@
-from tree_of_thoughts.models.openai_models import OpenAILanguageModel, OptimizedOpenAILanguageModel
-from tree_of_thoughts.treeofthoughts import TreeofThoughts, MonteCarloTreeofThoughts, TreeofThoughtsBFS, TreeofThoughtsDFS, TreeofThoughtsBEST, TreeofThoughtsASearch
-from tree_of_thoughts.models.abstract_language_model import AbstractLanguageModel
+from models.openai_model import OpenAILanguageModel, OptimizedOpenAILanguageModel
+from treeofthoughts import TreeofThoughts, MonteCarloTreeofThoughts, TreeofThoughtsBFS, TreeofThoughtsDFS, TreeofThoughtsBEST, TreeofThoughtsASearch
+from model.abstract_language_model import AbstractLanguageModel
 
-from tree_of_thoughts.models.huggingface_model import HuggingLanguageModel, HFPipelineModel
+from model.huggingface_model import HuggingLanguageModel, HFPipelineModel
 import re
