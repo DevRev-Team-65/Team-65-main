9a68f67686665704cc6abe6992c2d892f5d4ecff
AyushhS <ayush77.2000@gmail.com>
2023-12-12 19:47:24 +0530

    experiment branch house keeping

--- README.md
+++
@@ -1,3 +0,0 @@
-# Inter IIT Tech Meet 2023 PS - 4
-
-This is a repository of all the solutions of team 65 for PS-4 of Inter IIT Tech meet 2023.
--- composers/__init__.py
+++
@@ -1,7 +0,0 @@
-from cot_composer import ChainOfThoughtComposer
-from tot_composer import TreeOfThoughtComposer
-
-__all__ = [
-    'ChainOfThoughtComposer',
-    'TreeOfThoughtComposer'
-]
